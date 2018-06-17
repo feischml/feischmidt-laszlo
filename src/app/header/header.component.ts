a3013530c9a233560387f74247ea053e35bb53e1
feischml <metallaszlo@yahoo.com>
2018-06-17 14:56:25 +0300

    commit 17.07.2018

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer } from '@angular/core';
+import { SmoothScrollToDirective } from 'ng2-smooth-scroll';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  providers: [SmoothScrollToDirective]
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor( private scroll: SmoothScrollToDirective = new SmoothScrollToDirective(),
+               private element: ElementRef,
+               private renderer: Renderer
+  ){ }
 
   ngOnInit() {
+  }
+
+  private scrollTo(divId: String){
+    this.scroll.scrollTo = divId;
+    this.scroll.duration = 2500;
+    this.scroll.offset = 80;
+    // trigger the scroll
+    this.scroll.onClick();
+
+    // close the navbar
+    this.closeNavbar();
+  }
+
+  private closeNavbar(){
+    let navbar = this.element.nativeElement.querySelector('#navcol-1');
+    //navbar.collapse('hide');
+    console.log(navbar);
+    this.renderer.setElementClass(navbar, 'in', false);
+
+    //this.renderer.setElementClass(this.element.nativeElement.querySelector('#navcol-1'), 'collapse', hide); 
   }
 
 }
